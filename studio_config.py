3032142b8f08527bff435a8ece6b472e7b9d3395
NatalieCheong <50092072+NatalieCheong@users.noreply.github.com>
2025-09-12 13:43:27 +0800

    Delete studio_config.py

--- studio_config.py
+++
@@ -1,8 +0,0 @@
-from langgraph_economic_agent import LangGraphEconomicAgent
-
-# Export your graph for Studio
-agent = LangGraphEconomicAgent()
-graph = agent.graph
-
-# This makes it available to Studio
-__all__ = ["graph"]
